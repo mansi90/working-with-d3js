0b4d873721763187757602822dd6eda482312932
mansiarora <mansi@intelligrape.com>
2015-11-30 18:33:13 +0530

    Line Chart : Fixed issues with line graph; Added few more comments;

--- grails-app/assets/javascripts/lineChart.js
+++ grails-app/assets/javascripts/lineChart.js
@@ -1,58 +1,62 @@
-var drawLineChart = function () {
+function drawLineChart() {
 
     //Step 1 : Fetch Data
     var lineData = getLineData();
 
     // Step 2 : Add SVG
-    var margin = {top: 30, right: 30, bottom: 50, left: 50 },
-        width = 1000 - margin.left - margin.right, height = 500 - margin.top - margin.bottom;
+    var margin = {top: 50, right: 50, bottom: 50, left: 50}, width = 1000, height = 500;
+    createSvg(width, height);
 
-    var svg = createSvg(width, height, margin);
 
     // Step 3 : Define scale and axes
+    var vis = d3.select("#visualisation"),
 
-    var xScale = d3.scale.linear().range([margin.left, width+margin.left]).domain([d3.min(lineData, function (d) {
+        xRange = d3.scale.linear().range([margin.left, width - margin.right]).domain([d3.min(lineData, function (d) {
             return d.x;
-        }), d3.max(lineData, function (d) {
-            return d.x;
-        })]),
-        yScale = d3.scale.linear().range([height-margin.top, margin.top]).domain([d3.min(lineData, function (d) {
-            return d.y;
-        }), d3.max(lineData, function (d) {
-            return d.y;
-        })]);
+        }),
+            d3.max(lineData, function (d) {
+                return d.x;
+            })
+        ]),
 
-    var xAxis = d3.svg.axis().scale(xScale),
-        yAxis = d3.svg.axis().scale(yScale).orient('left'); // axis it needs to be oriented to the left
+        yRange = d3.scale.linear().range([height - margin.top, margin.bottom]).domain([d3.min(lineData, function (d) {
+            return d.y;
+        }),
+            d3.max(lineData, function (d) {
+                return d.y;
+            })
+        ]),
+
+        xAxis = d3.svg.axis().scale(xRange),
+        yAxis = d3.svg.axis().scale(yRange).orient('left'); // y-axis it needs to be oriented to the left
 
     //Step 4 :  Append both the axis to the SVG and apply the transformation
-    svg.append('g')  //g element is used to group SVG shapes together
-        .attr('class', 'x axis')
-        .attr('transform', 'translate(0,' + (height-margin.top) + ')')//The transforms are SVG transforms, check http://www.w3.org/TR/SVG/coords.html#TransformAttribute
+    vis.append("g")             //g element is used to group SVG shapes together
+        .attr("class", "x-axis")
+        .attr("transform", "translate(0," + (height - margin.bottom) + ")") //The transforms are SVG transforms, check http://www.w3.org/TR/SVG/coords.html#TransformAttribute
         .call(xAxis);
 
-    svg.append('g')
-        .attr('class', 'y axis')
-        .attr('transform', 'translate(' + (margin.left) + ',0)')
+    vis.append("g")
+        .attr("class", "y-axis")
+        .attr("transform", "translate(" + (margin.left) + ",0)")
         .call(yAxis);
-
     //We have transformed both the axes, keeping the defined margins in view so that the axes don’t touch the SVG margins.
-
 
     // Step 5 : Plot coordinates and draw a line.
     var lineFunc = d3.svg.line()
-        .x(function(d) {
-            return xScale(d.x);
+        .x(function (d) {
+            return xRange(d.x);
         })
-        .y(function(d) {
-            return xScale(d.y);
-        })
-        .interpolate('linear');  //The interpolate('linear') call tells D3 to draw straight lines.
-    svg.append('path')
-        .attr('d', lineFunc(lineData))
-        .attr('stroke', 'blue')
-        .attr('stroke-width', 1)
-        .attr('fill', 'none');
+        .y(function (d) {
+            return yRange(d.y);
+        });
+
+    vis.append("path")          //SVG Paths represent the outline of a shape that can be stroked, filled, used as a clipping path, or any combination of all three. We can draw rectangles, circles, ellipses, polylines, polygons, straight lines, and curves through path
+        .attr("d", lineFunc(lineData))
+        .attr("stroke", "blue")
+        .attr("stroke-width", 1)
+        .attr("fill", "none");
+
 };
 
 var getLineData = function () {
@@ -63,11 +67,11 @@ var getLineData = function () {
         },
         {
             x: 20,
-            y: 15
+            y: 90
         },
         {
             x: 40,
-            y: 60
+            y: 23
         },
         {
             x: 60,
@@ -84,10 +88,11 @@ var getLineData = function () {
     ];
 };
 
-var createSvg = function (width, height, margin) {
+var createSvg = function (width, height) {
     var svg = d3.select("#chartArea").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
+        .attr('id', 'visualisation')
+        .attr("width", width)
+        .attr("height", height)
         .style('background', '#ACBF70');
 
     return svg;
