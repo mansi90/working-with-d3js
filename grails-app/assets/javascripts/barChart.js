cf0c2b43ef4f84c20961ed5220e33b0e6579640d
mansiarora <mansi@intelligrape.com>
2015-11-30 22:38:27 +0530

    Bar chart : Updated logic;

--- grails-app/assets/javascripts/barChart.js
+++ grails-app/assets/javascripts/barChart.js
@@ -1,5 +1,5 @@
 var drawBarChart = function () {
-    //Step 1 : Fetch Data
+//Step 1 : Fetch Data
     var barData = getChartData();
 
     // Step 2 : Add SVG
@@ -9,25 +9,18 @@ var drawBarChart = function () {
     // Step 3 : Define scale and axes
     var vis = d3.select('#visualisation'),
 
-        xRange = d3.scale.linear().range([margin.left, width - margin.right]).domain([d3.min(barData, function (d) {
+        xRange = d3.scale.ordinal().rangeRoundBands([margin.left, width - margin.right], 0.1).domain(barData.map(function (d) {
             return d.x;
-        }),
-            d3.max(barData, function (d) {
-                return d.x;
-            })
-        ]),
+        })),
 
-        yRange = d3.scale.linear().range([height - margin.top, margin.bottom]).domain([d3.min(barData, function (d) {
-            return d.y;
-        }),
+
+        yRange = d3.scale.linear().range([height - margin.top, margin.bottom]).domain([0,
             d3.max(barData, function (d) {
                 return d.y;
             })
         ]),
 
-        xAxis = d3.svg.axis().scale(xRange),
-        yAxis = d3.svg.axis().scale(yRange).orient('left'); // y-axis it needs to be oriented to the left
-
+        xAxis = d3.svg.axis().scale(xRange), yAxis = d3.svg.axis().scale(yRange).orient("left");
 
     //Step 4 :  Append both the axis to the SVG and apply the transformation
     vis.append('g')
@@ -40,7 +33,6 @@ var drawBarChart = function () {
         .attr('transform', 'translate(' + (margin.left) + ',0)')
         .call(yAxis);
 
-
     // Step 5 Create rectangular bars for the chart data
     vis.selectAll('rect')
         .data(barData)
@@ -52,9 +44,18 @@ var drawBarChart = function () {
         .attr('y', function (d) { // sets the y position of the bar
             return yRange(d.y);
         })
-        .attr('width', 40) // sets the width of bar
-        .attr('height', function (d) {      // sets the height of bar
+        .attr('width', xRange.rangeBand()) // sets the width of bar
+        .attr('height', function (d) {  // sets the height of bar
             return ((height - margin.bottom) - yRange(d.y));
         })
-        .attr('fill', 'grey');   // fills the bar with grey color
+        .attr('fill', 'grey')   // fills the bar with grey color
+        .on('mouseover', function (d) {
+            d3.select(this)
+                .attr('fill', 'blue');
+        })
+        .on('mouseout', function (d) {
+            d3.select(this)
+                .attr('fill', 'grey');
+        });
+
 };
